fac4914b6ed03fa940e9914f362e537dff40c2aa
Alex Trinh <alex.trinh4@gmail.com>
2018-05-21 11:49:17 +1000

    added cors header to api

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, request
 from .data_publication import publication
 from .data_analytics import analytics
 
+def cors(resp):
+    resp.headers["Access-Control-Allow-Origin"] = '*'
+    request_headers = request.headers.get("Access-Control-Request-Headers")
+    resp.headers["Access-Control-Allow-Headers"] = request_headers
+    resp.headers['Access-Control-Allow-Methods'] = "DELETE, GET, POST, HEAD, OPTIONS"
+    return resp
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(publication)
 app.register_blueprint(analytics)
+app.after_request(cors)
