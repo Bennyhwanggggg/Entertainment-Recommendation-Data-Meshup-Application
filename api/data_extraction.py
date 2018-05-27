cf78e981b2c035681173c6c9d9cf156b3730ca1a
Benny Hwang <bennyhwangg@gmail.com>
2018-05-27 21:04:51 +1000

    book genre sorting refined

--- api/data_extraction.py
+++ api/data_extraction.py
@@ -70,7 +70,8 @@ def extract_book_data2(file='oldgoodbooks.csv'):
     raw_data = raw_data.drop('ratings_3', 1)
     raw_data = raw_data.drop('ratings_4', 1)
     raw_data = raw_data.drop('ratings_5', 1)
-    raw_data['average_rating'] = raw_data['average_rating'].apply(lambda x: x*2)
+    raw_data = raw_data.drop('average_rating', 1)
+    raw_data = raw_data.drop('revenue_test', 1)
     raw_data = raw_data.fillna('None')
     data = raw_data.to_dict(orient='records')
     return data
@@ -85,18 +86,97 @@ def dataimport():
         try:
             subjects = json_data[header]['subjects']
             genres = []
+            reject = ['nyt', 'New Your Time', 'Library', 'library', 'Accessible', 'Large', 'type', 'Print', 'print', 'Internet', 'Archive', 'Wishlist', 'strips']
+            fiction = ['Troll', 'Fiction', 'Batman', 'Spy', 'Spies']
+            thriller = ['Homicide', 'Abusive', 'abusive', 'Thriller', 'Psychology', 'Horror', 'Detectives', 'detectives', 'criminal', 'Criminal', 'Secret', 'Violence']
+            comic = ['comic', 'Graphic']
+            sen = ['Horror comic books', 'Japan', 'Comic', 'attack', 'Fate']
+            literature = ['English', 'century', 'Literature', 'Britain', 'British' 'Revolution', 'Translation', 'translation', 'relation', 'Historical', 'historical', 'academic']
+            war = ['War', 'World', 'Ship']
+            dra = ['Relations', 'Romance', 'Family', 'friendship', 'Female', 'Friendship', 'Revenge', 'Teenage', 'drama', 'Uncle', 'uncle', 'Grand', 'student', 'School', 'school']
+            fant = ['Ambition', 'Dragon', 'Fantasy', 'Dream', 'Heroes', 'Imaginary', 'creature', 'Titan', 'Sailor', 'fantasy']
+            life = ['Life', 'parent', 'Parent', 'Lifestyle', 'life', 'Experience']
+            comedy = ['funny', 'Jokes', 'Dream', 'Circus']
+            scifi = ['Time', 'Science', 'Chemistry', 'Space', 'technology', 'space', 'explore']
+            supernatur = ['Supernatural', 'Vampires', 'Evil', 'Spirituality']
+            superpow = ['Royalty', 'tale', 'God']
             for subject in subjects:
-                genres.append(subject['name'])
+                if any(word in subject['name'] for word in reject):
+                    continue
+                elif any(word in subject['name'] for word in fiction):
+                    genres.append('Fiction')
+                elif any(word in subject['name'] for word in thriller):
+                    genres.append('Thriller')
+                elif any(word in subject['name'] for word in comic):
+                    genres.append('Shounen')
+                elif any(word in subject['name'] for word in sen):
+                    genres.append('Seinen')
+                elif any(word in subject['name'] for word in literature):
+                    genres.append('Literature')
+                elif any(word in subject['name'] for word in war):
+                    genres.append('Military')
+                elif any(word in subject['name'] for word in dra):
+                    genres.append('Drama')
+                elif any(word in subject['name'] for word in fant):
+                    genres.append('Fantasy')
+                elif any(word in subject['name'] for word in comedy):
+                    genres.append('Comedy')
+                elif any(word in subject['name'] for word in life):
+                    genres.append('Slice of Life')
+                elif any(word in subject['name'] for word in scifi):
+                    genres.append('Sci-Fi')
+                elif any(word in subject['name'] for word in supernatur):
+                    genres.append('Supernatural')
+                elif any(word in subject['name'] for word in superpow):
+                    genres.append('Super Power')
+                elif subject['name'] not in genres and not subject['name'].isdigit() and subject['name'].isalpha():
+                    genres.append(subject['name'])
             genres = ', '.join(genres)
         except:
             genres = 'Undefined'
+        if not genres or genres == ' ' or genres == 'Undefined':
+            genres = []
+            fiction_list = ['King', 'Lord', 'Harry', 'Star', 'Kill', 'Letter', 'Games', 'Animal', 'Hobbit', 'Game', 'Great', 'Forgot', 'Dreams', 'Maze']
+            if any(word in data['title'] for word in fiction_list):
+                genres.append('Fiction')
+            super_list = ['Super', 'power', 'Power', 'Mighty', 'mighty']
+            if any(word in data['title'] for word in super_list):
+                genres.append('Super Power')
+            fantasylist = ['Dragon', 'Sun', 'Magic', 'Adventure', 'adventure', 'journey', 'Journey', 'Tale', 'tale', 'Legend', 'legend', 'giant', 'Giant', 'Dream']
+            if any(word in data['title'] for word in fantasylist):
+                genres.append('Fantasy')
+            thrillerlist = ['Missing', 'Murderer', 'Grey', 'kill', 'Power', 'power', 'Death', 'death']
+            if any(word in data['title'] for word in thrillerlist):
+                genres.append('Thriller')
+            machinelist = ['Machine', 'machine', 'robot', 'Robot', 'iron', 'Iron', 'metal', 'Metal']
+            if any(word in data['title'] for word in machinelist):
+                genres.append('Mecha')
+            scifilist = ['Time', 'time']
+            if any(word in data['title'] for word in scifilist):
+                genres.append('Sci-FI')
+            dramalist = ['Secret', 'boy', 'Girl', 'Moon', 'girl', 'moon', 'Family', 'family', 'home', 'Love', 'Lovely']
+            if any(word in data['title'] for word in dramalist):
+                genres.append('Drama')
+            litlist = ['Women', 'men', 'Men', 'City', 'Art']
+            if any(word in data['title'] for word in litlist):
+                genres.append('Literature')
+            supernat_list = ['Spirit', 'spirit']
+            if any(word in data['title'] for word in supernat_list):
+                genres.append('Super Natural')
+            if genres:
+                genres = ', '.join(genres)
+        if not genres:
+            genres = 'Undefined'
+
         data['genre'] = genres
         try:
             data['isbn'] = float(data['isbn'])
-            print(data)
-            Books(data['title'], data['authors'], data['average_rating'],
-                data['isbn'], data['isbn13'], data['original_publication_year'], data['image_url'], data['small_image_url'], data['genre']).save()
-        except:
+            print(data['title'], data['genre'])
+            Books(data['title'], data['authors'], data['rating'],
+                data['isbn'], data['isbn13'], data['original_publication_year'], data['image_url'], data['small_image_url'], data['genre'], data['revenue']).save()
+        except Exception as e:
+            print(e)
+
             continue
     print('done!')
 
